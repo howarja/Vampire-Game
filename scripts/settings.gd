78790e799ed924569dd88cae2f9df166f3328205
HelloIsReal <60082945+HelloIsReal@users.noreply.github.com>
2026-08-25 17:15:04 +1200

    Master volume slider functions, test volume button
    - Made the volume slider change the master volume
    - Added test volume button (Josh Hutcherson whistle. Change for copyright later probably)

--- scripts/settings.gd
+++ scripts/settings.gd
@@ -5,3 +5,8 @@ func _ready() -> void:
 
 func _on_volume_slider_value_changed(value: float) -> void:
 	Globals.currentVolume = value
+	AudioServer.set_bus_volume_linear(AudioServer.get_bus_index("Master"), Globals.currentVolume)
+
+
+func _on_test_volume_pressed() -> void:
+	$testPlayer.play(6.7)

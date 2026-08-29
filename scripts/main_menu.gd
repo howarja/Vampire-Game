09794e58dc82d88c9192428a9045da99ecf433c9
Zero <Zachary.f.quinn@gmail.com>
2026-08-29 12:34:31 +1200

    Sorted some files

--- scripts/main_menu.gd
+++ scripts/main_menu.gd
@@ -1,12 +1,18 @@
 extends Node2D
 
-@onready var normalText = $TextHolder/normalText
+@onready var normal_text = $TextHolder/normalText
+@onready var top_text = $TextHolder/topText
+@onready var bottom_text = $TextHolder/bottomText
+@onready var game_text = $TextHolder/gameText
+
+@onready var settings = $Settings
+@onready var exit_button = $exitButton
 
 func _ready() -> void:
-	$TextHolder/gameText.visible=false
-	$Settings.visible = false
-	$exitButton.visible=false
-	normalText.modulate.a = 0
+	game_text.visible=false
+	settings.visible = false
+	exit_button.visible=false
+	normal_text.modulate.a = 0
 	fadeText("GAME PRODUCED BY",2.5)
 	await get_tree().create_timer(5).timeout
 	fadeText("James Howard\nJoshua Wolf\nTishar Sreekantam\nTijmen Verstappen\nDr. Zachavious XII Quinn the 2nd. Sr",3)
@@ -14,40 +20,40 @@ func _ready() -> void:
 	logoAnimation()
 
 func fadeText(newText,visibleLength):
-	normalText.text = newText
+	normal_text.text = newText
 	var tween = get_tree().create_tween()
-	tween.tween_property(normalText, "modulate:a", 1, visibleLength).set_trans(Tween.TRANS_CUBIC)
+	tween.tween_property(normal_text, "modulate:a", 1, visibleLength).set_trans(Tween.TRANS_CUBIC)
 	await tween.finished
 	
-	normalText.text = newText
+	normal_text.text = newText
 	tween = get_tree().create_tween()
-	tween.tween_property(normalText, "modulate:a", 0, visibleLength).set_trans(Tween.TRANS_CUBIC)
+	tween.tween_property(normal_text, "modulate:a", 0, visibleLength).set_trans(Tween.TRANS_CUBIC)
 	await tween.finished
 
 func logoAnimation():
 	var tween = get_tree().create_tween().set_parallel(true)
-	tween.tween_property($TextHolder/topText, "position", Vector2(0,-240), 4).set_trans(Tween.TRANS_CUBIC)
-	tween.tween_property($TextHolder/topText, "rotation", deg_to_rad(2520), 4).set_trans(Tween.TRANS_QUAD)
+	tween.tween_property(top_text, "position", Vector2(0,-240), 4).set_trans(Tween.TRANS_CUBIC)
+	tween.tween_property(top_text, "rotation", deg_to_rad(2520), 4).set_trans(Tween.TRANS_QUAD)
 	
-	tween.tween_property($TextHolder/bottomText, "position", Vector2(0,240), 4).set_trans(Tween.TRANS_CUBIC)
-	tween.tween_property($TextHolder/bottomText, "rotation", deg_to_rad(-2520), 4).set_trans(Tween.TRANS_QUAD)
+	tween.tween_property(bottom_text, "position", Vector2(0,240), 4).set_trans(Tween.TRANS_CUBIC)
+	tween.tween_property(bottom_text, "rotation", deg_to_rad(-2520), 4).set_trans(Tween.TRANS_QUAD)
 	await tween.finished
 	
 	tween = get_tree().create_tween().set_parallel(true)
-	tween.tween_property($TextHolder/topText, "position", Vector2(0,-60), 0.2).set_trans(Tween.TRANS_CUBIC)
-	tween.tween_property($TextHolder/bottomText, "position", Vector2(0,60), 0.2).set_trans(Tween.TRANS_CUBIC)
+	tween.tween_property(top_text, "position", Vector2(0,-60), 0.2).set_trans(Tween.TRANS_CUBIC)
+	tween.tween_property(bottom_text, "position", Vector2(0,60), 0.2).set_trans(Tween.TRANS_CUBIC)
 	await tween.finished
 	
-	$TextHolder/topText.visible=false
-	$TextHolder/bottomText.visible=false
+	top_text.visible=false
+	bottom_text.visible=false
 	
-	$TextHolder/gameText.visible=true
-	$exitButton.visible=true
-	$Settings.visible = true
+	game_text.visible=true
+	exit_button.visible=true
+	settings.visible = true
 
 
 func _on_button_pressed() -> void:
-	get_tree().change_scene_to_file("res://startScene.tscn")
+	get_tree().change_scene_to_file("res://scenes/startScene.tscn")
 
 
 func _on_exit_button_pressed() -> void:

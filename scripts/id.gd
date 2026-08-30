ea507100a5d31bb966d2e65b5f958b2914fa134b
Zero <Zachary.f.quinn@gmail.com>
2026-08-30 19:17:18 +1200

    Id set up

---
+++ scripts/id.gd
@@ -0,0 +1,24 @@
+extends Control
+class_name id
+
+@onready var name_label: Label = $TextureRect/Name;
+@onready var date_of_birth_label: Label = $TextureRect/DateOfBirth;
+@onready var address_label: Label = $TextureRect/Address;
+@onready var height_label: Label = $TextureRect/Height;
+@onready var weight_label: Label = $TextureRect/Weight;
+@onready var hair_label: Label = $TextureRect/Hair;
+@onready var eyes_label: Label = $TextureRect/Eyes;
+@onready var social_security_label: Label = $"TextureRect/Social Security";
+
+var currentCharacter: character;
+
+func update_id(person: character):
+	currentCharacter = person;
+	name_label.text = person.fullName;
+	date_of_birth_label.text = person.information.dateOfBirth;
+	address_label.text = person.information.address;
+	height_label.text = person.information.bringing;
+	weight_label.text = person.information.bloodType;
+	hair_label.text = person.information.lifeStatus;
+	#eyes_label
+	#social_security_label

07b24771d1f1304388d453b4a7b88efe2aaf0c6f
Zero <Zachary.f.quinn@gmail.com>
2026-08-31 17:45:51 +1200

    False id stuff works

--- scripts/id.gd
+++ scripts/id.gd
@@ -14,11 +14,49 @@ var currentCharacter: character;
 
 func update_id(person: character):
 	currentCharacter = person;
-	name_label.text = person.fullName;
-	date_of_birth_label.text = person.information.dateOfBirth;
-	address_label.text = person.information.address;
-	height_label.text = person.information.height;
-	weight_label.text = person.information.weight;
-	hair_label.text = person.information.hair_color;
-	eyes_label.text = person.information.eye_color;
-	social_security_label.text = person.information.scocial_security;
+	if(person.fake_information != null):
+		name_label.text = person.fullName;
+		
+		if person.fake_information.dateOfBirth != null:
+			date_of_birth_label.text = person.fake_information.dateOfBirth;
+		else:
+			date_of_birth_label.text = person.information.dateOfBirth;
+		
+		if person.fake_information.address != null:
+			address_label.text = person.fake_information.address;
+		else:
+			address_label.text = person.information.address;
+		
+		if person.fake_information.height != null:
+			height_label.text = person.fake_information.height;
+		else:
+			height_label.text = person.information.height;
+		
+		if person.fake_information.weight != null:
+			height_label.text = person.fake_information.weight;
+		else:
+			height_label.text = person.information.weight;
+		
+		if person.fake_information.hair_color != null:
+			height_label.text = person.fake_information.hair_color;
+		else:
+			height_label.text = person.information.hair_color;
+		
+		if person.fake_information.eye_color != null:
+			height_label.text = person.fake_information.eye_color;
+		else:
+			height_label.text = person.information.eye_color;
+		
+		if person.fake_information.scocial_security != null:
+			height_label.text = person.fake_information.scocial_security;
+		else:
+			height_label.text = person.information.scocial_security;
+	else:
+		name_label.text = person.fullName;
+		date_of_birth_label.text = person.information.dateOfBirth;
+		address_label.text = person.information.address;
+		height_label.text = person.information.height;
+		weight_label.text = person.information.weight;
+		hair_label.text = person.information.hair_color;
+		eyes_label.text = person.information.eye_color;
+		social_security_label.text = person.information.scocial_security;
